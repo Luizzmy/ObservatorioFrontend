174de3009cf1eb586430a100f00dfa819deacaad
Luizzmy <luisg.valenzuelam@gmail.com>
2021-06-01 15:31:59 -0500

    busqueda de articulos por palabras clave

--- src/components/BarraYResultados.js
+++ src/components/BarraYResultados.js
@@ -1,17 +1,20 @@
 import React, { useState, useEffect } from 'react'
-import { Input, AutoComplete } from 'antd';
-import { SelectProps } from 'antd/es/select';
+import { Input, AutoComplete, Menu, message, Typography } from 'antd';
 import ListaResultados from './ListaResultados';
 import CollapseEstados from './CollapseEstados';
 import ResultadosCompArt from './ResultadosCompArt';
+import { DownOutlined, UserOutlined } from '@ant-design/icons';
 
+const { Text } = Typography
 function BarraYResultados({ tipo }) {
 
     const [words, setWords] = useState(null)
     const [data, setData] = useState()
+    const [leyes, setLeyes] = useState()
+    const [tipoBusq, settipoBusq] = useState()
 
     useEffect(() => {
-        function getEstados() {
+        function getData() {
             fetch('/articulos.json')
                 .then(function (response) {
                     return response.json()
@@ -21,13 +24,13 @@ function BarraYResultados({ tipo }) {
                 })
 
         }
-        getEstados()
+        getData()
     }, [])
 
     const placeholders = (
         tipo === "leyes" ? "Buscar por título" :
             tipo === "estado" ? "Buscar por estado" :
-                tipo === "articulo" ? "Buscar por palabra clave" :
+                tipo === "articulo" ? "Busque artículos  por palabra clave" :
                     null
     )
 
@@ -40,17 +43,74 @@ function BarraYResultados({ tipo }) {
         setWords(e.target.value)
     };
 
+    const handleLeyChange = e => {
+        setWords(e)
+    };
+
+    const handleArtChange = e => {
+        setWords(e)
+    };
+
+    const handleArtSearch = (value) => {
+        setWords(value)
+        console.log(value)
+    }
+
+
+    useEffect(() => {
+        function makeMenu(data) {
+            let leyes = []
+            let res = []
+            let n = 0
+            for (let i in data) {
+                leyes.push(...Object.keys(data[i]))
+            }
+            leyes.forEach(e => {
+                n = n + 1
+                res.push(
+                    {value:e}
+                )
+            })
+            setLeyes(res)
+        }
+
+        if (data) {
+            makeMenu(data)
+        }
+    }, [data])
 
     return (
         <div>
-            <Input.Search size="large" placeholder={placeholders} onChange={handleChange} onSearch={handleSearch} enterButton />
+            <Input.Search size="large" placeholder={placeholders} onSearch={handleSearch} enterButton />
+            <br/>
+            <br/>
+            {tipo === "articulo" && leyes ?
+                <div>
+                    <Text>Ó seleccione una ley y busque por número de artículo</Text>
+                    <br />
+                    <AutoComplete
+                        style={{
+                            width: "100%",
+                            marginBottom:"5px"
+                        }}
+                        options={leyes}
+                        onChange={handleLeyChange}
+                        placeholder="Busque una ley por nombre"
+                        filterOption={(inputValue, option) =>
+                            option.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+                        }
+                    />
+                    <Input.Search size="large" placeholder="No. de articulo" onChange={handleArtChange} onSearch={handleArtSearch} enterButton />
+                </div>
+                :
+                null}
             {tipo === "leyes" ?
                 <ListaResultados tipo={tipo} words={words} />
                 :
-                tipo==="estado" ?
-                <CollapseEstados tipo={tipo} words={words} data={data} />
-                :
-                <ResultadosCompArt data={data}/>
+                tipo === "estado" ?
+                    <CollapseEstados tipo={tipo} words={words} data={data} />
+                    :
+                    <ResultadosCompArt data={data} words={words} tipoBusq={tipoBusq} />
 
             }
         </div>
--- src/components/ResultadosCompArt.js
+++ src/components/ResultadosCompArt.js
@@ -1,11 +1,67 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import {Col, Row, List, Typography} from 'antd'
 
-function ResultadosCompArt({words, data}) {
+const{Text}=Typography
 
+function ResultadosCompArt({words, data, tipoBusq}) {
+
+    const [articulos, setArticulos] = useState()
+
+    useEffect(() => {
+        function filterData(data, words){
+            let res=[]
+            if(words){
+                let wordsarr=words.split(" ")
+            for(let i in data){
+                for(let j in data[i]){
+                    for (let k in data[i][j]){
+                        if(wordsarr.every(w=>data[i][j][k].texto.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+                        .includes(w.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")))){
+                            res.push({
+                                estado:i,
+                                ley:j,
+                                articulo:k,
+                                texto:data[i][j][k].texto
+                            })
+                        }
+                    }
+                    }
+                }
+                setArticulos(res)
+            }
+        }
+        filterData(data, words)
+    }, [words, data])
 
     return (  
         <div>
-            
+            {articulos ?
+            <>
+            <Text>Artículos que incluyen las palabras: {words}</Text>
+            <List
+    itemLayout="horizontal"
+    dataSource={articulos}
+    renderItem={item => (
+      <List.Item>
+        <List.Item.Meta
+          title={item.articulo}
+          description={
+              <>
+          <Text>Entidad:<b>{item.estado}</b></Text><br/>
+          <Text>Ley/Reglamento:<b>{item.ley}</b></Text><br/>
+          <Text>{item.texto}</Text><br/>
+          </>
+          }
+        />
+      </List.Item>
+    )}
+  /> 
+  </>
+:
+null            
+        }
+
+
             
         </div>
     )
